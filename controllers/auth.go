636c0c55cde875b0a4a8502aa47afe9569853ec3
fajarAnd <fajarandriansyah1988@gmail.com>
2025-06-15 15:27:30 +0700

    Login Auth endpoint

--- controllers/auth.go
+++ controllers/auth.go
@@ -1,1 +1,137 @@
 package controller
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/fajar-andriansyah/loan-engine/models"
+	"github.com/fajar-andriansyah/loan-engine/usecase"
+	"github.com/go-playground/validator/v10"
+	"github.com/rs/zerolog/log"
+)
+
+type AuthController struct {
+	authUsecase usecase.AuthUsecase
+	validator   *validator.Validate
+}
+
+func NewAuthController(authUsecase usecase.AuthUsecase) *AuthController {
+	return &AuthController{
+		authUsecase: authUsecase,
+		validator:   validator.New(),
+	}
+}
+
+func (c *AuthController) Login(w http.ResponseWriter, r *http.Request) {
+	var req models.LoginRequest
+
+	// Parse request body
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		log.Error().Err(err).Msg("Failed to decode request body")
+		c.sendErrorResponse(w, http.StatusBadRequest, "Invalid request body", nil)
+		return
+	}
+
+	// Validate request
+	if err := c.validator.Struct(&req); err != nil {
+		log.Error().Err(err).Msg("Validation failed")
+		c.sendValidationErrorResponse(w, err)
+		return
+	}
+
+	// Process login
+	response, err := c.authUsecase.Login(r.Context(), &req)
+	if err != nil {
+		log.Error().Err(err).Str("email", req.Email).Str("user_type", req.UserType).Msg("Login failed")
+
+		// Determine error type and status code
+		if err.Error() == "invalid credentials" || err.Error() == "authentication failed" {
+			c.sendErrorResponse(w, http.StatusUnauthorized, "Invalid credentials", map[string]string{
+				"error_code": "INVALID_CREDENTIALS",
+			})
+		} else {
+			c.sendErrorResponse(w, http.StatusInternalServerError, "Internal server error", map[string]string{
+				"error_code": "INTERNAL_ERROR",
+			})
+		}
+		return
+	}
+
+	log.Info().Str("email", req.Email).Str("user_type", req.UserType).Msg("Login successful")
+	c.sendSuccessResponse(w, http.StatusOK, "Login successful", response)
+}
+
+func (c *AuthController) sendSuccessResponse(w http.ResponseWriter, statusCode int, message string, data interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+
+	response := models.Response[interface{}]{
+		Data: map[string]interface{}{
+			"success": true,
+			"message": message,
+			"data":    data,
+		},
+	}
+
+	json.NewEncoder(w).Encode(response)
+}
+
+func (c *AuthController) sendErrorResponse(w http.ResponseWriter, statusCode int, message string, extra map[string]string) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+
+	errorData := map[string]interface{}{
+		"success": false,
+		"message": message,
+	}
+
+	for k, v := range extra {
+		errorData[k] = v
+	}
+
+	response := models.Response[interface{}]{
+		Data: errorData,
+	}
+
+	json.NewEncoder(w).Encode(response)
+}
+
+func (c *AuthController) sendValidationErrorResponse(w http.ResponseWriter, err error) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusBadRequest)
+
+	var errors []map[string]string
+	for _, err := range err.(validator.ValidationErrors) {
+		fieldError := map[string]string{
+			"field":   err.Field(),
+			"message": getValidationMessage(err),
+		}
+		errors = append(errors, fieldError)
+	}
+
+	response := models.Response[interface{}]{
+		Data: map[string]interface{}{
+			"success": false,
+			"message": "Validation error",
+			"errors":  errors,
+		},
+	}
+
+	json.NewEncoder(w).Encode(response)
+}
+
+func getValidationMessage(err validator.FieldError) string {
+	switch err.Tag() {
+	case "required":
+		return fmt.Sprintf("%s is required", err.Field())
+	case "email":
+		return "Invalid email format"
+	case "min":
+		return fmt.Sprintf("%s must be at least %s characters", err.Field(), err.Param())
+	case "oneof":
+		return fmt.Sprintf("%s must be one of: %s", err.Field(), err.Param())
+	default:
+		return fmt.Sprintf("%s is invalid", err.Field())
+	}
+}
--- infrastructure/http/router/router.go
+++ infrastructure/http/router/router.go
@@ -1,18 +1,38 @@
 package router
 
 import (
-	_healthController "github.com/fajar-andriansyah/loan-engine/controllers"
+	"github.com/fajar-andriansyah/loan-engine/controllers"
+	"github.com/fajar-andriansyah/loan-engine/infrastructure/database"
+	"github.com/fajar-andriansyah/loan-engine/repositories"
+	"github.com/fajar-andriansyah/loan-engine/usecase"
 	"github.com/go-chi/chi/v5"
+	"github.com/spf13/viper"
 )
 
 func GetRouter() chi.Router {
 	r := chi.NewRouter()
 
-	// Middleware
-	// TODO: Set Middleware
+	// Initialize dependencies
+	db := database.GetConn()
+
+	// Repositories
+	authRepo := repositories.NewAuthRepository(db)
+
+	// Usecases
+	jwtSecret := viper.GetString("jwt.secret")
+	authUsecase := usecase.NewAuthUsecase(authRepo, jwtSecret)
+
+	// Controllers
+	authController := controller.NewAuthController(authUsecase)
 
 	// Routes
-	r.Get("/__health", _healthController.GetHealth)
+	r.Get("/__health", controller.GetHealth)
+
+	// API v1 routes
+	r.Route("/api/v1", func(r chi.Router) {
+		// Auth routes
+		r.Post("/auth/login", authController.Login)
+	})
 
 	return r
 }
