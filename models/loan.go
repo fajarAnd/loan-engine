0614556f742a2cde6c9414f59db4b58b6d9cbddc
fajarAnd <fajarandriansyah1988@gmail.com>
2025-06-15 20:37:57 +0700

    add model approveloan response

--- models/loan.go
+++ models/loan.go
@@ -35,3 +35,24 @@ type Loan struct {
 	CreatedAt       time.Time `json:"created_at"`
 	UpdatedAt       time.Time `json:"updated_at"`
 }
+
+type ApproveLoanRequest struct {
+	ApprovalNotes string `json:"approval_notes"`
+}
+
+type ApproveLoanResponse struct {
+	ID                       uuid.UUID `json:"id"`
+	BorrowerID               uuid.UUID `json:"borrower_id"`
+	PrincipalAmount          float64   `json:"principal_amount"`
+	InterestRate             float64   `json:"interest_rate"`
+	ROIRate                  float64   `json:"roi_rate"`
+	LoanTermMonth            int       `json:"loan_term_month"`
+	CurrentState             string    `json:"current_state"`
+	ApprovalDate             string    `json:"approval_date"`
+	ApprovingEmployeeID      uuid.UUID `json:"approving_employee_id"`
+	ApprovalNotes            string    `json:"approval_notes,omitempty"`
+	LoanAgreementPDFURL      string    `json:"loan_agreement_pdf_url"`
+	FieldValidatorEmployeeID uuid.UUID `json:"field_validator_employee_id"`
+	SurveyDate               string    `json:"survey_date"`
+	UpdatedAt                time.Time `json:"updated_at"`
+}
